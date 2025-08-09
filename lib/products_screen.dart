d111eeb7aaf0d3c21b4273870b9ddd0af49e5c98
Muhammad-Nazih <mohamednazih840@gmail.com>
2025-08-09 23:31:16 +0300

    5th commit

--- lib/products_screen.dart
+++ lib/products_screen.dart
@@ -5,6 +5,38 @@ class ProductsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Products Screen'));
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(color: Colors.blue, width: 200, height: 200),
+                  Container(color: Colors.red, width: 200, height: 200),
+                  Container(color: Colors.green, width: 200, height: 200),
+                  Container(color: Colors.black, width: 200, height: 200),
+                  Container(color: Colors.yellow, width: 200, height: 200),
+                ],
+              ),
+            ),
+            GridView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: 10,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemBuilder: (context, indexx) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(height: 150, width: 150, color: Colors.blue),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
