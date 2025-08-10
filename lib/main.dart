413c203bc95ce1e7678c07b2f0f22c5cfca9b7ab
Muhammad-Nazih <mohamednazih840@gmail.com>
2025-08-10 16:00:47 +0300

    6th commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http_app/login_screen.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.white,
+  ));
   runApp(MyApp());
 }
 
